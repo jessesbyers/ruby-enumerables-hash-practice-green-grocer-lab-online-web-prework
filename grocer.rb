40455444926048b3c4e4385bb00e555d5a4fa284
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-06 15:30:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,17 +44,24 @@ def apply_clearance(cart)
     cart
 end
 
-def checkout(cart, coupons)
-  consolidate_cart(cart)
-  apply_coupons(cart, coupons)
-  cart.each do |total_itemcost| 
-    [:price] * [:count]
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) {||
+  
+ 
+end  
+  
+ # cart.each do |total_itemcost| 
+ #   [:price] * [:count]
     
-  end
-  total = final_hash[element_name][:total_itemcost].sum
-  total
-  end
-    
+ # end
+ # total = final_hash[element_name][:total_itemcost].sum
+ # total
+ # end
+  
+  
   # ccalculate total cost of unsorted cart
   # apply array of coupons
   # use 3 methods already written
